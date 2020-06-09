87d2be08d2c76920669092094b801fad2262bc91
au619265 <au619265@uni.au.dk>
2020-06-09 22:20:10 +0200

    Created some foundation, like nav bar

---
+++ lib/Pages/main_page.dart
@@ -0,0 +1,54 @@
+import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
+import 'package:flutter/material.dart';
+import 'NavBarWidget/games_library_widget.dart';
+import 'NavBarWidget/home_page_widget.dart';
+import 'NavBarWidget/settings_widget.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 1;
+  final List<Widget> _children = [
+    GamesLibraryWidget(),
+    HomePageWidget(),
+    SettingsWidget()
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          automaticallyImplyLeading: true,
+          title: Text("What is in my Kitchen"),
+        ),
+        body: Stack(children: <Widget>[
+          _children[_currentIndex],
+        ],
+        ),
+        // This trailing comma makes auto-formatting nicer for build methods.
+        bottomNavigationBar: FancyBottomNavigation(
+          tabs:[
+            TabData(iconData:Icons.gamepad, title: "Games"),
+            TabData(iconData: Icons.access_time, title: "Home"),
+            TabData(iconData: Icons.settings, title: "Settings")
+          ],
+          onTabChangedListener: (int position) {
+            setState(() {
+              _currentIndex = position;
+            });
+          },
+          initialSelection: 1,
+          circleColor: Colors.orange,
+        )
+    );
+  }
+}
